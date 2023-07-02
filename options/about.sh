10020a209fde34d9b330d2d24e1834041e450fbb
csvpndev <128266371+csvpndev@users.noreply.github.com>
2023-07-02 17:33:15 +0700

    Update about.sh

--- options/about.sh
+++ options/about.sh
@@ -16,14 +16,14 @@ echo -e "│               AUTOSCRIPT PREMIUM                │" | lolcat
 echo -e "│             BY COBEK SAWIT TUNNELL              │" | lolcat
 echo -e "└─────────────────────────────────────────────────┘" | lolcat
 
-echo -e ">Support OS 
-For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
-For Debian 9 & Debian 10 & Debian 11 " | lolcat
-
-echo -e "Contact Owner AutoScript
+echo -e "> Support OS 
+  For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+  For Debian 9 & Debian 10 & Debian 11 " | lolcat
+echo ""
+echo -e "  Contact Owner AutoScript
 > WA: 083831678849
 > TELEGRAM: t.me/cobeksawitofficial" | lolcat
-
+echo ""
 echo -e "Thanks for your Order Cobek Sawit Tunnel" | lolcat
 echo ""
 read -n 1 -s -r -p "  Press any key to Back to Menu"
