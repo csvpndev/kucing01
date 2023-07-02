fcca6904ed085d7863acf885948e72cbb163844d
csvpndev <128266371+csvpndev@users.noreply.github.com>
2023-07-02 14:10:57 +0700

    Add files via upload

---
+++ options/about.sh
@@ -0,0 +1,29 @@
+ echo -e "    
+
+   _____ ____  ____  ______ _  __   _____    __          _______ _______ 
+  / ____/ __ \|  _ \|  ____| |/ /  / ____|  /\ \        / /_   _|__   __|
+ | |   | |  | | |_) | |__  | ' /  | (___   /  \ \  /\  / /  | |    | |   
+ | |   | |  | |  _ <|  __| |  <    \___ \ / /\ \ \/  \/ /   | |    | |   
+ | |___| |__| | |_) | |____| . \   ____) / ____ \  /\  /   _| |_   | |   
+  \_____\____/|____/|______|_|\_\ |_____/_/    \_\/  \/   |_____|  |_|   
+                                                                         
+                                                                         
+ " | lolcat
+
+
+echo -e ">>>>>>>>>>Welcome to AutoScript Premium V2<<<<<<<<<<
+>>>>>>>>>>>>>>>>>>AUTOSCRIPT BY Cobek Sawit Tunnel<<<<<<<<<<<<<<<<<" | lolcat
+
+echo -e ">Support OS 
+For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+For Debian 9 & Debian 10 & Debian 11 " | lolcat
+
+echo -e "Contact Owner AutoScript
+> WA: 083831678849
+> TELEGRAM: t.me/cobeksawitofficial" | lolcat
+
+echo -e "Thanks for your Order Arz AutoScript" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
