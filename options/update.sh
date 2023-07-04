1d24ef53dd57b63be9cb168ac3278bb0e7ab151e
csvpndev <128266371+csvpndev@users.noreply.github.com>
2023-07-04 16:56:07 +0700

    Update update.sh

--- options/update.sh
+++ options/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #wget https://github.com/${GitUser}/
-GitUser="csvpn"
+GitUser="csvpndev"
 if [ "${EUID}" -ne 0 ]; then
                 echo "You need to run this script as root"
                 exit 1
